e392a6238dae5884b1b432ade7fe0cacc88da5de
AndonMitev <mitevandon94@gmail.com>
2023-02-16 22:13:12 +0200

    remove unused files

--- src/components/BoxContents/MintedSymbolsForWave/index.jsx
+++ src/components/BoxContents/MintedSymbolsForWave/index.jsx
@@ -1,14 +1,13 @@
 import { useQuery } from 'react-query';
 
 import { fetchSymbolsForWave } from '../../../services/fetchSymbolsInfo';
-import { Box } from '../../Box';
-import { BoxHeader } from '../../BoxHeader';
+
 import { Loader } from '../../ContentLoader';
 import { Table } from '../../Table';
 
 export const MintedSymbolsForWave = () => {
   const { data, isLoading } = useQuery('mintedSymbols', fetchSymbolsForWave);
-  console.log();
+
   return isLoading ? (
     <Loader />
   ) : (
--- src/screens/PlayersTable/index.jsx
+++ src/screens/PlayersTable/index.jsx
@@ -1,5 +1,3 @@
-import { Table } from '../../components/Table';
-
 export const PlayersTable = () => {
   return <div>{/* <Table /> */}</div>;
 };
